01b82e1e451335bc18679ab974387e1e16207b5f
Sagthy <santirem3@gmail.com>
2024-02-20 21:20:17 -0300

    add aria labels

--- src/components/DatePickerElement.jsx
+++ src/components/DatePickerElement.jsx
@@ -3,13 +3,15 @@ import DatePicker from 'react-datepicker';
 export function DatePickerElement({ startDate, handleDateChange, openDatePicker, setOpenDatePicker, wrapperRef }) {
     return (
         <div ref={wrapperRef} style={{ position: 'absolute' }}>
+            <label htmlFor="datePicker">Date Picker</label>
             <DatePicker
+                id="datePicker"
                 selected={startDate}
                 onChange={handleDateChange}
                 open={openDatePicker}
                 onCalendarClose={() => setOpenDatePicker(false)}
                 onCalendarOpen={() => setOpenDatePicker(true)}
-                customInput={<div />}
+                customInput={<div aria-label="Date Picker" />}
             />
         </div>
     );

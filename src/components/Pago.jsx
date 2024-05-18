01b8aad9723a45daa090e96b22f643c9fae124ba
Santiago Remedi <santirem3@gmail.com>
2024-05-18 17:25:44 -0300

    chore: Refactor Pago component and import statements

--- src/components/Pago.jsx
+++ src/components/Pago.jsx
@@ -1,50 +1,64 @@
 /* eslint-disable react/prop-types */
-import { usePago } from '../logic/usePago'
-import { SwitchElement } from './SwitchElement'
-import { DatePickerElement } from './DatePickerElement'
-import { StartDateElement } from './StartDateElement'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPencilAlt } from '@fortawesome/free-solid-svg-icons'
-import '../styles/Pago.css'
 
-import PaymentModal from './modals/PaymentModal'
+import { usePago } from '../logic/usePago';
+import { SwitchElement } from './SwitchElement';
+import { DatePickerElement } from './DatePickerElement';
+import { StartDateElement } from './StartDateElement';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPencilAlt } from '@fortawesome/free-solid-svg-icons';
+import '../styles/Pago.css';
+import PaymentModal from './modals/PaymentModal';
+import { useDisclosure } from '@chakra-ui/react';
 
-import { useDisclosure } from '@chakra-ui/react'
+export function Pago({ pago, mes }) {
+  const { checked, startDate, openDatePicker, handleChange, handleDateChange, dateDifference, setOpenDatePicker } = usePago(pago, mes);
 
-export function Pago ({ pago, mes }) {
-  const { checked, startDate, openDatePicker, handleChange, handleDateChange, dateDifference, setOpenDatePicker } = usePago(pago, mes)
-
-  let itemStyle = ''
+  let itemStyle = '';
   if (checked) {
-    itemStyle = 'item-green'
+    itemStyle = 'item-green';
   } else if (!startDate || dateDifference > 3) {
-    itemStyle = 'item-gray'
+    itemStyle = 'item-gray';
   } else if (dateDifference < 0) {
-    itemStyle = 'item-red'
+    itemStyle = 'item-red';
   } else if (dateDifference <= 3) {
-    itemStyle = 'item-orange'
+    itemStyle = 'item-orange';
   }
 
-  const { isOpen, onOpen, onClose } = useDisclosure()
-
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
-    <li className={itemStyle}  onClick={onOpen}>
-      <span >
+    <li className={itemStyle} onClick={onOpen}>
+      <span>
         {pago.nombre}
         <SwitchElement checked={checked} handleChange={handleChange} />
         <div style={{ position: 'relative', padding: '0.2rem 0.4rem' }}>
-          <button onClick={(e) => {
-            e.stopPropagation()
-            setOpenDatePicker(!openDatePicker)
-            }} aria-label='Open Date Picker'>
+          <button
+            onClick={(e) => {
+              e.stopPropagation();
+              setOpenDatePicker(!openDatePicker);
+            }}
+            aria-label='Open Date Picker'
+          >
             <FontAwesomeIcon icon={faPencilAlt} />
           </button>
-          {openDatePicker && <DatePickerElement startDate={startDate} handleDateChange={handleDateChange} openDatePicker={openDatePicker} setOpenDatePicker={setOpenDatePicker}/>}
+          {openDatePicker && (
+            <div
+              onClick={(e) => {
+                e.stopPropagation();
+              }}
+            >
+              <DatePickerElement
+                startDate={startDate}
+                handleDateChange={handleDateChange}
+                openDatePicker={openDatePicker}
+                setOpenDatePicker={setOpenDatePicker}
+              />
+            </div>
+          )}
         </div>
       </span>
       <StartDateElement checked={checked} startDate={startDate} />
       <PaymentModal isOpen={isOpen} onClose={onClose} paymentInfo={pago} fechaVencimiento={startDate} mes={mes} />
     </li>
-  )
+  );
 }

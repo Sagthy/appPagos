92187f3515edcb6ee04797def836d0661b6e9af7
Sxnty <sxntydev@gmail.com>
2024-05-18 14:22:14 -0300

    chore: refactor App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,146 +1,82 @@
 // eslint-disable-next-line no-redeclare
 /* global localStorage */
-import { useState } from 'react'
-import 'react-datepicker/dist/react-datepicker.css'
-import { Mes } from './components/Mes'
-import './styles/App.css'
+import { useState } from "react";
+import "react-datepicker/dist/react-datepicker.css";
+import { Mes } from "./components/Mes";
 
-const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
+import payments from "./constants/payments.constants";
 
-const pagosConst = [
-  {
-    nombre: 'Patente',
-    mensual: false,
-    meses: ['Enero']
-  },
-  {
-    nombre: 'Contribución Salto',
-    mensual: false,
-    meses: ['Febrero']
-  },
-  {
-    nombre: 'Contribución Mdeo',
-    mensual: false,
-    meses: ['Marzo', 'Julio', 'Noviembre']
-  },
+import "./styles/App.css";
+import "react-datepicker/dist/react-datepicker.css";
 
-  {
-    nombre: 'Seguro',
-    mensual: false,
-    meses: ['Octubre', 'Noviembre', 'Diciembre']
-  },
-  {
-    nombre: 'Primaria',
-    mensual: false,
-    meses: ['Mayo', 'Agosto', 'Octubre']
-  },
-  {
-    nombre: 'Universidad',
-    mensual: true
-  },
-  {
-    nombre: 'UTE Salto',
-    mensual: true
-  },
-  {
-    nombre: 'UTE Mdeo',
-    mensual: true
-  },
-  {
-    nombre: 'OSE',
-    mensual: true
-  },
-  {
-    nombre: 'Cardio',
-    mensual: true
-  },
-  {
-    nombre: 'Gastos comunes Salto',
-    mensual: true
-  },
-  {
-    nombre: 'Gastos comunes Mdeo',
-    mensual: true
-  },
-  {
-    nombre: 'OCA',
-    mensual: true
-  },
-  {
-    nombre: 'Cable',
-    mensual: true
-  },
-  {
-    nombre: 'Netflix',
-    mensual: true
-  },
-  {
-    nombre: 'Centro Médico',
-    mensual: true
-  },
-  {
-    nombre: 'Veterinaria',
-    mensual: true
-  },
-  {
-    nombre: 'Teléfono',
-    mensual: true
-  },
-  {
-    nombre: 'Caja Profesional',
-    mensual: true
-  },
-  {
-    nombre: 'Sereno',
-    mensual: true
-  }
-]
+const meses = [
+  "Enero",
+  "Febrero",
+  "Marzo",
+  "Abril",
+  "Mayo",
+  "Junio",
+  "Julio",
+  "Agosto",
+  "Septiembre",
+  "Octubre",
+  "Noviembre",
+  "Diciembre",
+];
 
 const App = () => {
-  const [indiceInicio, setIndiceInicio] = useState(new Date().getMonth() - 1 < 0 ? 0 : new Date().getMonth() - 1)
+  const [indiceInicio, setIndiceInicio] = useState(
+    new Date().getMonth() - 1 < 0 ? 0 : new Date().getMonth() - 1
+  );
 
-  const mostrarMeses = () => {
-    return meses.slice(indiceInicio, indiceInicio + 3).map((mes) => (
-      <Mes key={mes} mes={mes} pagos={pagosConst} />
-    ))
-  }
+  const showedMonths = meses.slice(indiceInicio, indiceInicio + 3);
 
   const handleNextClick = () => {
     if (indiceInicio + 1 < meses.length - 2) {
-      setIndiceInicio(indiceInicio + 1)
+      setIndiceInicio(indiceInicio + 1);
     }
-  }
+  };
 
   const handlePreviousClick = () => {
     if (indiceInicio - 1 >= 0) {
-      setIndiceInicio(indiceInicio - 1)
+      setIndiceInicio(indiceInicio - 1);
     }
-  }
+  };
 
   const handleResetClick = () => {
-    localStorage.clear()
-    window.location.reload()
-  }
+    localStorage.clear();
+    window.location.reload();
+  };
 
   return (
     <main>
-      <h1>PAGOS PENDIENTES
-        <br />💸💸💸💸<br />
+      <h1>
+        PAGOS PENDIENTES
+        <br />
+        💸💸💸💸
+        <br />
       </h1>
-      <div className='buttonContainer'>
-        <button className='myButton' onClick={handlePreviousClick}>Anterior</button>
-        <button className='myButton' onClick={handleNextClick}>Siguiente</button>
+      <div className="buttonContainer">
+        <button className="myButton" onClick={handlePreviousClick}>
+          Anterior
+        </button>
+        <button className="myButton" onClick={handleNextClick}>
+          Siguiente
+        </button>
       </div>
-      <section className='mesesYPagos'>
-        {mostrarMeses()}
+      <section className="mesesYPagos">
+        {showedMonths.map((mes) => (
+          <Mes key={mes} mes={mes} pagos={payments} />
+        ))}
       </section>
-      <div className='buttonContainer'>
-        <button className='myButton-reset' onClick={handleResetClick}>Reset</button>
+      <div className="buttonContainer">
+        <button className="myButton-reset" onClick={handleResetClick}>
+          Reset
+        </button>
       </div>
-      <div className='absolute' />
-
+      <div className="absolute" />
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
